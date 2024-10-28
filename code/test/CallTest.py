0520568277a7eed4efef81ebf39d77fefd8d55c9
wisdom <1120231246@bit.edu.cn>
2024-10-28 23:45:23 +0800

    transform

---
+++ code/test/CallTest.py
@@ -0,0 +1,9 @@
+class Person:
+    def __call__(self,name):
+        print("__call__"+"Hello"+name)
+
+    def hello(self,name):
+        print("Hello"+ name)
+
+person = Person()
+person.hello("Tom")
