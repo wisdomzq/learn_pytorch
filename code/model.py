0d6f68f56779baf47ff488af07cb311a6fdc6a6e
wisdom <1120231246@bit.edu.cn>
2024-11-27 00:09:57 +0800

    现有模型的使用修改及保存读取

---
+++ code/model.py
@@ -0,0 +1,20 @@
+from torch import nn
+
+
+class Wisdom(nn.Module):
+    def __init__(self):
+        super(Wisdom, self).__init__()
+        self.model = nn.Sequential(
+            nn.Conv2d(3, 32, 5, padding=2),
+            nn.MaxPool2d(2),
+            nn.Conv2d(32, 32, 5, padding=2),
+            nn.MaxPool2d(2),
+            nn.Conv2d(32, 64, 5, padding=2),
+            nn.MaxPool2d(2),
+            nn.Flatten(),
+            nn.Linear(64 * 4 * 4, 64),
+            nn.Linear(64, 10)
+        )
+
+    def forward(self, x):
+        return self.model(x)
